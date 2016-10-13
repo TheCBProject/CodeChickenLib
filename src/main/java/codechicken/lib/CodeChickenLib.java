5aecfc3df9c7a468f892a3addb88ae56fb57ae71
covers1624 <laughlan.cov@internode.on.net>
2016-10-13 16:38:12 +1030

    Move target MC version for mods to CCL.

--- src/main/java/codechicken/lib/CodeChickenLib.java
+++ src/main/java/codechicken/lib/CodeChickenLib.java
@@ -9,12 +9,13 @@ import net.minecraftforge.fml.relauncher.Side;
 /**
  * Created by covers1624 on 12/10/2016.
  */
-@Mod(modid = CodeChickenLib.MOD_ID, name = CodeChickenLib.MOD_NAME)
+@Mod(modid = CodeChickenLib.MOD_ID, name = CodeChickenLib.MOD_NAME, acceptedMinecraftVersions = CodeChickenLib.mcVersion)
 public class CodeChickenLib {
 
     public static final String MOD_ID = "CodeChickenLib";
     public static final String MOD_NAME = "CodeChicken Lib";
     public static final String version = "${mod_version}";
+    public static final String mcVersion = "[1.10.2]";
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
