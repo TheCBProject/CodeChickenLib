15060724d5be392c0b09668bf56906d79f9aed6e
covers1624 <laughlan.cov@internode.on.net>
2016-11-03 09:47:31 +1030

    Fix baking CCModel baking pipe.

--- src/main/java/codechicken/lib/model/bakery/CCModelBakery.java
+++ src/main/java/codechicken/lib/model/bakery/CCModelBakery.java
@@ -1,6 +1,8 @@
 package codechicken.lib.model.bakery;
 
 import codechicken.lib.render.CCModel;
+import codechicken.lib.render.CCRenderState;
+import codechicken.lib.render.buffer.BakingVertexBuffer;
 import codechicken.lib.render.pipeline.IVertexOperation;
 import codechicken.lib.render.pipeline.IVertexSource;
 import net.minecraft.client.renderer.block.model.BakedQuad;
@@ -22,12 +24,11 @@ public class CCModelBakery {
      * Assumes you want to bake the entire model.
      *
      * @param model  Model to bake.
-     * @param sprite Sprite the quads are to be baked from.
      * @param ops    Any Operations to apply.
      * @return The BakedQuads for the model.
      */
-    public static List<BakedQuad> bakeModel(CCModel model, TextureAtlasSprite sprite, IVertexOperation... ops) {
-        return bakeModel(model, sprite, 0, model.getVertices().length, ops);
+    public static List<BakedQuad> bakeModel(CCModel model, IVertexOperation... ops) {
+        return bakeModel(model, 0, model.getVertices().length, ops);
     }
 
     /**
@@ -35,14 +36,13 @@ public class CCModelBakery {
      * Assumes DefaultVertexFormats.BLOCK.
      *
      * @param model  Model to bake.
-     * @param sprite Sprite the quads are to be baked from.
      * @param start  The first vertex index to bake.
      * @param end    The Vertex index to bake until.
      * @param ops    Any Operations to apply.
      * @return The BakedQuads for the model.
      */
-    public static List<BakedQuad> bakeModel(CCModel model, TextureAtlasSprite sprite, int start, int end, IVertexOperation... ops) {
-        return bakeModel(model, DefaultVertexFormats.BLOCK, sprite, start, end, ops);
+    public static List<BakedQuad> bakeModel(CCModel model, int start, int end, IVertexOperation... ops) {
+        return bakeModel(model, DefaultVertexFormats.ITEM, start, end, ops);
     }
 
     /**
@@ -51,12 +51,11 @@ public class CCModelBakery {
      *
      * @param model  Model to bake.
      * @param format VertexFormat to bake to.
-     * @param sprite Sprite the quads are to be baked from.
      * @param ops    Any Operations to apply.
      * @return The BakedQuads for the model.
      */
-    public static List<BakedQuad> bakeModel(CCModel model, VertexFormat format, TextureAtlasSprite sprite, IVertexOperation... ops) {
-        return bakeModel(model, format, sprite, 0, model.getVertices().length, ops);
+    public static List<BakedQuad> bakeModel(CCModel model, VertexFormat format, IVertexOperation... ops) {
+        return bakeModel(model, format, 0, model.getVertices().length, ops);
     }
 
     /**
@@ -64,14 +63,13 @@ public class CCModelBakery {
      *
      * @param model  Vertex source to bake.
      * @param format VertexFormat to bake to.
-     * @param sprite Sprite the quads are to be baked from.
      * @param start  The first vertex index to bake.
      * @param end    The Vertex index to bake until.
      * @param ops    Any Operations to apply.
      * @return The BakedQuads for the model.
      */
-    public static List<BakedQuad> bakeModel(CCModel model, VertexFormat format, TextureAtlasSprite sprite, int start, int end, IVertexOperation... ops) {
-        return bakeModel(model, model.vp == 3, format, sprite, start, end, ops);
+    public static List<BakedQuad> bakeModel(CCModel model, VertexFormat format, int start, int end, IVertexOperation... ops) {
+        return bakeModel(model, model.vp == 3, format, start, end, ops);
     }
 
     /**
@@ -79,41 +77,19 @@ public class CCModelBakery {
      *
      * @param model       Vertex source to bake.
      * @param isTriangles Specifies if the model's vertices are triangles.
-     * @param format      VertexFormat to bake to.
-     * @param sprite      Sprite the quads are to be baked from.
+     * @param format      VertexFormat to bake to, You almost always want this to be ITEM.
      * @param start       The first vertex index to bake.
      * @param end         The Vertex index to bake until.
      * @param ops         Any Operations to apply.
      * @return The BakedQuads for the model.
      */
-    public static List<BakedQuad> bakeModel(IVertexSource model, boolean isTriangles, VertexFormat format, TextureAtlasSprite sprite, int start, int end, IVertexOperation... ops) {
-        //The face for quads is assigned down the line using the quads normals.
-        CCQuadBakery bakery = new CCQuadBakery(format, sprite);
-        if (isTriangles) {
-            bakery.startBakingTriangles();
-        } else {
-            bakery.startBakingQuads();
-        }
-        /*CCRenderState.fmt = format;
-        CCRenderState.setPipeline(model, start, end, ops);
-        Vertex5[] verts = model.getVertices();
-        for (vertexIndex = firstVertexIndex; vertexIndex < lastVertexIndex; vertexIndex++) {
-            model.prepareVertex();
-            vert.set(verts[vertexIndex]);
-            runPipeline();
-            if (CCRenderState.sprite != null) {
-                try {
-                    bakery.setSprite(CCRenderState.sprite);
-                } catch (IllegalStateException e) {
-                    throw new RuntimeException("Unable to set sprite in baking pipeline! Please ensure your IconVertexRangeUVTransform is correct!", e);
-                }
-            }
-            Vertex5 vert = CCRenderState.vert;
-            bakery.setColour(CCRenderState.colour);
-            bakery.setLightMap(CCRenderState.brightness);
-            bakery.setNormal(CCRenderState.normal);
-            bakery.addVertexWithUV(vert);
-        }*/
-        return bakery.finishBaking();
+    public static List<BakedQuad> bakeModel(IVertexSource model, boolean isTriangles, VertexFormat format, int start, int end, IVertexOperation... ops) {
+        BakingVertexBuffer buffer = BakingVertexBuffer.create();
+        buffer.begin(isTriangles ? 4 : 7, format);
+        CCRenderState ccrs = CCRenderState.instance();
+        ccrs.bind(buffer);
+        ccrs.setPipeline(model, start, end, ops);
+        ccrs.render();
+        return buffer.bake();
     }
 }
