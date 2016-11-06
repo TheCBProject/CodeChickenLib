e90ed0430fd4d03a0e2f9be6fb7b60b0c00d6689
P3pp3rF1y <p3pp3rf1y@gmail.com>
2016-11-06 23:24:53 +0100

    Fixed playing sound for entities on server.

--- src/main/java/codechicken/lib/util/SoundUtils.java
+++ src/main/java/codechicken/lib/util/SoundUtils.java
@@ -2,6 +2,7 @@ package codechicken.lib.util;
 
 import codechicken.lib.vec.Vector3;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.world.World;
@@ -16,7 +17,14 @@ public class SoundUtils {
     }
 
     public static void playSoundAt(Entity entity, SoundCategory category, SoundEvent sound, float volume, float pitch) {
-        playSoundAt(entity, category, sound, volume, pitch, false);
+        EntityPlayer player = null;
+
+        //only assigning player on client as if triggered on server the player passed here would be omitted from hearing the sound
+        if (entity.worldObj.isRemote && entity instanceof EntityPlayer) {
+            player = (EntityPlayer) entity;
+        }
+
+        entity.worldObj.playSound(player, entity.posX, entity.posY, entity.posZ, sound, category, volume, pitch);
     }
 
     public static void playSoundAt(Entity entity, SoundCategory category, SoundEvent sound) {
