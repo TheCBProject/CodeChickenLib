5231d0351bd596a2745d35a8e3f4033cf42b3ad1
covers1624 <laughlan.cov@internode.on.net>
2016-11-21 15:04:32 +1030

    IParticleProvider and IType, used for Enum type properties.

---
+++ src/main/java/codechicken/lib/block/IType.java
@@ -0,0 +1,16 @@
+package codechicken.lib.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.util.IStringSerializable;
+
+/**
+ * Created by covers1624 on 20/11/2016.
+ */
+public interface IType extends IStringSerializable {
+
+    IProperty<?> getTypeProperty();
+
+    int meta();
+
+}
