be49b81d8cf447c0c512b48b00bb33f1c1b7b5a9
covers1624 <laughlan.cov@internode.on.net>
2016-12-09 21:37:37 +1030

    Move property string to use a linked list.

--- src/main/java/codechicken/lib/block/property/PropertyString.java
+++ src/main/java/codechicken/lib/block/property/PropertyString.java
@@ -1,40 +1,36 @@
 package codechicken.lib.block.property;
 
 import com.google.common.base.Optional;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Lists;
+import com.google.common.collect.ImmutableList;
 import net.minecraft.block.properties.PropertyHelper;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by covers1624 on 2/6/2016.
  */
 public class PropertyString extends PropertyHelper<String> {
 
-    private final HashSet<String> valuesSet;
+    private final List<String> valuesSet;
 
     public PropertyString(String name, Collection<String> values) {
         super(name, String.class);
-        valuesSet = new HashSet<String>(values);
+        valuesSet = new LinkedList<String>(values);
     }
 
     public PropertyString(String name, String... values) {
         super(name, String.class);
-        valuesSet = new HashSet<String>();
+        valuesSet = new LinkedList<String>();
         Collections.addAll(valuesSet, values);
     }
 
     public List<String> values() {
-        return Lists.newLinkedList(valuesSet);
+        return ImmutableList.copyOf(valuesSet);
     }
 
     @Override
     public Collection<String> getAllowedValues() {
-        return ImmutableSet.copyOf(valuesSet);
+        return ImmutableList.copyOf(valuesSet);
     }
 
     @Override
