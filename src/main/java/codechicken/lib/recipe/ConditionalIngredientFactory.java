78a2cac8a2a691186538742fcef678bee533ca1e
covers1624 <laughlan.cov@internode.on.net>
2017-11-09 23:40:25 +1030

    Some Recipe stuffs

---
+++ src/main/java/codechicken/lib/recipe/ConditionalIngredientFactory.java
@@ -0,0 +1,30 @@
+package codechicken.lib.recipe;
+
+import com.google.gson.JsonObject;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.JsonUtils;
+import net.minecraftforge.common.crafting.CraftingHelper;
+import net.minecraftforge.common.crafting.IIngredientFactory;
+import net.minecraftforge.common.crafting.JsonContext;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Simple conditional ingredient.
+ *
+ * Created by covers1624 on 18/10/2017.
+ */
+public class ConditionalIngredientFactory implements IIngredientFactory {
+
+    @Nonnull
+    @Override
+    public Ingredient parse(JsonContext context, JsonObject json) {
+        if (!CraftingHelper.processConditions(JsonUtils.getJsonArray(json, "conditions"), context)) {
+            if (json.has("fail")) {
+                CraftingHelper.getIngredient(JsonUtils.getJsonObject(json, "failed_condition"), context);
+            }
+            return Ingredient.EMPTY;
+        }
+        return CraftingHelper.getIngredient(JsonUtils.getJsonObject(json, "pass"), context);
+    }
+}
