879d6ec38d4d1313706660d5f2de0ef375e3799b
covers1624 <laughlan.cov@internode.on.net>
2020-05-12 18:15:18 +0930

    Fix chunk load hook causing chunk loading deadlocks.

--- src/main/java/codechicken/lib/world/TileChunkLoadHook.java
+++ src/main/java/codechicken/lib/world/TileChunkLoadHook.java
@@ -1,10 +1,16 @@
 package codechicken.lib.world;
 
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.ChunkPrimer;
 import net.minecraft.world.chunk.ChunkPrimerWrapper;
 import net.minecraft.world.chunk.IChunk;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.ChunkEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import java.util.Map;
 
 public class TileChunkLoadHook {
 
@@ -22,13 +28,22 @@ public class TileChunkLoadHook {
     @SubscribeEvent
     public void onChunkLoad(ChunkEvent.Load event) {
         IChunk chunk = event.getChunk();
-        if(chunk instanceof ChunkPrimerWrapper) {
+        Map<BlockPos, TileEntity> tiles = null;
+        if (chunk instanceof ChunkPrimerWrapper) {
             chunk = ((ChunkPrimerWrapper) chunk).func_217336_u();
         }
-        chunk.getTileEntitiesPos().stream()//
-                .map(chunk::getTileEntity)//
-                .filter(e -> e instanceof IChunkLoadTile)//
-                .map(e -> (IChunkLoadTile) e)//
-                .forEach(IChunkLoadTile::onChunkLoad);
+        if (chunk instanceof Chunk) {
+            tiles = ((Chunk) chunk).getTileEntityMap();
+        }
+        if (chunk instanceof ChunkPrimer) {
+            tiles = ((ChunkPrimer) chunk).getTileEntities();
+        }
+        if (tiles != null) {
+            for (TileEntity tile : tiles.values()) {
+                if (tile instanceof IChunkLoadTile) {
+                    ((IChunkLoadTile) tile).onChunkLoad();
+                }
+            }
+        }
     }
 }
