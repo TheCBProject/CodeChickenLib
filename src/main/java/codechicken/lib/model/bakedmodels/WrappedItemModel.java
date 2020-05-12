f061fc45aa5fdbaaf36d04a199294fd9f790815c
covers1624 <laughlan.cov@internode.on.net>
2020-05-12 18:15:34 +0930

    Revive WrappedItemModel

--- src/main/java/codechicken/lib/model/bakedmodels/WrappedItemModel.java
+++ src/main/java/codechicken/lib/model/bakedmodels/WrappedItemModel.java
@@ -1,69 +1,76 @@
-//package codechicken.lib.model.bakedmodels;
-//
-//import codechicken.lib.model.ModelRegistryHelper;
-//import net.minecraft.block.BlockState;
-//import net.minecraft.client.Minecraft;
-//import net.minecraft.client.renderer.ItemRenderer;
-//import net.minecraft.client.renderer.model.BakedQuad;
-//import net.minecraft.client.renderer.model.IBakedModel;
-//import net.minecraft.client.renderer.model.ItemOverrideList;
-//import net.minecraft.client.renderer.model.ModelResourceLocation;
-//import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-//import net.minecraft.entity.LivingEntity;
-//import net.minecraft.item.ItemStack;
-//import net.minecraft.util.Direction;
-//import net.minecraft.world.World;
-//
-//import javax.annotation.Nullable;
-//import java.util.Collections;
-//import java.util.List;
-//import java.util.Random;
-//import java.util.function.Supplier;
-//
-///**
-// * A simple wrapper item model.
-// * Created by covers1624 on 5/07/2017.
-// */
-//public abstract class WrappedItemModel implements IBakedModel {
-//
-//    protected IBakedModel wrapped;
-//    @Nullable
-//    protected LivingEntity entity;
-//    @Nullable
-//    protected World world;
-//
-//    public WrappedItemModel(IBakedModel wrapped) {
-//        this.wrapped = wrapped;
-//    }
-//
-//    @Override
-//    public List<BakedQuad> getQuads(BlockState state, Direction side, Random rand) {
-//        return Collections.emptyList();
-//    }
-//
-//    @Override
-//    public TextureAtlasSprite getParticleTexture() {
-//        return null;
-//    }
-//
-//    @Override
-//    public ItemOverrideList getOverrides() {
-//        return overrideList;
-//    }
-//
-//    protected void renderWrapped(ItemStack stack) {
-//        IBakedModel model = wrapped.getOverrides().getModelWithOverrides(wrapped, stack, world, entity);
-//        ItemRenderer renderItem = Minecraft.getInstance().getItemRenderer();
-//        renderItem.renderModel(model, stack);
-//    }
-//
-//    private ItemOverrideList overrideList = new ItemOverrideList() {
-//        @Override
-//        public IBakedModel getModelWithOverrides(IBakedModel originalModel, ItemStack stack, @Nullable World world, @Nullable LivingEntity entity) {
-//            WrappedItemModel.this.entity = entity;
-//            WrappedItemModel.this.world = world == null ? entity == null ? null : entity.world : null;
-//            return originalModel;
-//        }
-//    };
-//
-//}
+package codechicken.lib.model.bakedmodels;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.*;
+import net.minecraft.client.renderer.model.BakedQuad;
+import net.minecraft.client.renderer.model.IBakedModel;
+import net.minecraft.client.renderer.model.ItemCameraTransforms.TransformType;
+import net.minecraft.client.renderer.model.ItemOverrideList;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Direction;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+
+/**
+ * A simple wrapper item model.
+ * Created by covers1624 on 5/07/2017.
+ */
+public abstract class WrappedItemModel implements IBakedModel {
+
+    protected IBakedModel wrapped;
+    @Nullable
+    protected LivingEntity entity;
+    @Nullable
+    protected World world;
+
+    private final ItemOverrideList overrideList = new ItemOverrideList() {
+        @Override
+        public IBakedModel getModelWithOverrides(IBakedModel originalModel, ItemStack stack, @Nullable World world, @Nullable LivingEntity entity) {
+            WrappedItemModel.this.entity = entity;
+            WrappedItemModel.this.world = world == null ? entity == null ? null : entity.world : null;
+            return originalModel;
+        }
+    };
+
+    public WrappedItemModel(IBakedModel wrapped) {
+        this.wrapped = wrapped;
+    }
+
+    @Override
+    public List<BakedQuad> getQuads(BlockState state, Direction side, Random rand) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public TextureAtlasSprite getParticleTexture() {
+        return null;
+    }
+
+    @Override
+    public ItemOverrideList getOverrides() {
+        return overrideList;
+    }
+
+    protected void renderWrapped(ItemStack stack, TransformType transformType, MatrixStack mStack, IRenderTypeBuffer getter, int packedLight, int packedOverlay) {
+        IBakedModel model = wrapped.getOverrides().getModelWithOverrides(wrapped, stack, world, entity);
+
+        ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
+        RenderType rType = RenderTypeLookup.getRenderType(stack);
+        if (transformType == TransformType.GUI && Objects.equals(rType, Atlases.getTranslucentBlockType())) {
+            rType = Atlases.getTranslucentCullBlockType();
+        }
+
+        IVertexBuilder builder = ItemRenderer.getBuffer(getter, rType, true, stack.hasEffect());
+        itemRenderer.renderModel(model, stack, packedLight, packedOverlay, mStack, builder);
+    }
+}
