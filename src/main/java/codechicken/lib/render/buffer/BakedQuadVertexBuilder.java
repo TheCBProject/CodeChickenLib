e9531246fa0659ad1bbabcb157f461093d5443ec
KitsuneAlex <alexh@karmakrafts.dev>
2021-08-24 10:40:49 +0200

    Fix formatting mistakes, add check for building state in endVertex & add newline to gradle.properties

--- src/main/java/codechicken/lib/render/buffer/BakedQuadVertexBuilder.java
+++ src/main/java/codechicken/lib/render/buffer/BakedQuadVertexBuilder.java
@@ -22,156 +22,168 @@ import java.util.List;
  */
 public class BakedQuadVertexBuilder implements IVertexBuilder, ISpriteAwareVertexBuilder {
 
-	private final List<Quad> quadList = new ArrayList<>();
-	private final int glMode;
-	private final int vSize;
+    private final List<Quad> quadList = new ArrayList<>();
+    private final int glMode;
+    private final int vSize;
 
-	private CachedFormat format = CachedFormat.lookup(DefaultVertexFormats.BLOCK);
-	private Quad current;
-	private int vertex;
+    private CachedFormat format = CachedFormat.lookup(DefaultVertexFormats.BLOCK);
+    private Quad current;
+    private int vertex;
 
-	public BakedQuadVertexBuilder() {
-		this(GL11.GL_QUADS);
-	}
+    public BakedQuadVertexBuilder() {
+        this(GL11.GL_QUADS);
+    }
 
-	public BakedQuadVertexBuilder(int glMode) {
-		if (glMode != GL11.GL_QUADS && glMode != GL11.GL_TRIANGLES) {
-			throw new IllegalArgumentException("Only GL_QUADS and GL_TRIANGLES supported. Got: " + glMode);
+    public BakedQuadVertexBuilder(int glMode) {
+        if (glMode != GL11.GL_QUADS && glMode != GL11.GL_TRIANGLES) {
+            throw new IllegalArgumentException("Only GL_QUADS and GL_TRIANGLES supported. Got: " + glMode);
+        }
+
+        this.glMode = glMode;
+        vSize = glMode == GL11.GL_QUADS ? 4 : 3;
+    }
+
+    public void reset() {
+        quadList.clear();
+        current = null;
+        vertex = 0;
+    }
+
+    public void setFormat(@Nonnull VertexFormat format) {
+        this.format = CachedFormat.lookup(format);
+    }
+
+    @Override
+    public void sprite(TextureAtlasSprite sprite) {
+        checkNewQuad();
+        current.setTexture(sprite);
+    }
+
+    @Nonnull
+    @Override
+    public IVertexBuilder vertex(double x, double y, double z) {
+        if (!format.hasPosition) {
+            return this;
+        }
+
+        checkNewQuad();
+        current.vertices[vertex].vec[0] = (float) x;
+        current.vertices[vertex].vec[1] = (float) y;
+        current.vertices[vertex].vec[2] = (float) z;
+        return this;
+    }
+
+	@Nonnull
+    @Override
+    public IVertexBuilder color(int red, int green, int blue, int alpha) {
+        if (!format.hasColor) {
+            return this;
+        }
+
+        checkNewQuad();
+        current.vertices[vertex].color[0] = red / 255F;
+        current.vertices[vertex].color[1] = green / 255F;
+        current.vertices[vertex].color[2] = blue / 255F;
+        current.vertices[vertex].color[3] = alpha / 255F;
+        return this;
+    }
+
+	@Nonnull
+    @Override
+    public IVertexBuilder uv(float u, float v) {
+        if (!format.hasUV) {
+            return this;
+        }
+
+        checkNewQuad();
+        current.vertices[vertex].uv[0] = u;
+        current.vertices[vertex].uv[1] = v;
+        return this;
+    }
+
+	@Nonnull
+    @Override
+    public IVertexBuilder overlayCoords(int u, int v) {
+        if (!format.hasUV) {
+            return this;
+        }
+
+        checkNewQuad();
+        current.vertices[vertex].overlay[0] = u / (float) 0xF0;
+        current.vertices[vertex].overlay[1] = v / (float) 0xF0;
+        return this;
+    }
+
+	@Nonnull
+    @Override
+    public IVertexBuilder uv2(int u, int v) {
+        if (!format.hasUV) {
+            return this;
+        }
+
+        checkNewQuad();
+        current.vertices[vertex].lightmap[0] = u / (float) 0xF0;
+        current.vertices[vertex].lightmap[1] = v / (float) 0xF0;
+        return this;
+    }
+
+	@Nonnull
+    @Override
+    public IVertexBuilder normal(float x, float y, float z) {
+        if (!format.hasNormal) {
+            return this;
+        }
+
+        checkNewQuad();
+        current.vertices[vertex].normal[0] = x;
+        current.vertices[vertex].normal[1] = y;
+        current.vertices[vertex].normal[2] = z;
+        return this;
+    }
+
+    @Override
+    public void endVertex() {
+    	if(current == null) {
+    		throw new IllegalStateException("Not building.");
 		}
+    	
+        vertex++;
+        if (vertex == vSize) {
+            if (glMode == GL11.GL_TRIANGLES) {
+                //Quadulate.
+                for (int e = 0; e < current.format.elementCount; e++) {
+                    System.arraycopy(current.vertices[2].raw[e], 0, current.vertices[3].raw[e], 0, 4);
+                }
+            }
 
-		this.glMode = glMode;
-		vSize = glMode == GL11.GL_QUADS ? 4 : 3;
-	}
+            if (current.sprite == null) {
+                throw new IllegalStateException("Sprite not set.");
+            }
 
-	public void reset() {
-		quadList.clear();
-		current = null;
-		vertex = 0;
-	}
+            quadList.add(current);
+            current = null;
+            vertex = 0;
+        }
+    }
 
-	public void setFormat(@Nonnull VertexFormat format) {
-		this.format = CachedFormat.lookup(format);
-	}
+    public List<BakedQuad> bake() {
+        if (current != null) {
+            throw new IllegalStateException("Not finished building.");
+        }
 
-	@Override
-	public void sprite(TextureAtlasSprite sprite) {
-		checkNewQuad();
-		current.setTexture(sprite);
-	}
+        final ArrayList<BakedQuad> quads = new ArrayList<>();
 
-	@Override
-	public IVertexBuilder vertex(double x, double y, double z) {
-		if (!format.hasPosition) {
-			return this;
-		}
+        for (final Quad unbakedQuad : quadList) {
+            quads.add(unbakedQuad.bake());
+        }
 
-		checkNewQuad();
-		current.vertices[vertex].vec[0] = (float) x;
-		current.vertices[vertex].vec[1] = (float) y;
-		current.vertices[vertex].vec[2] = (float) z;
-		return this;
-	}
+        return quads;
+    }
 
-	@Override
-	public IVertexBuilder color(int red, int green, int blue, int alpha) {
-		if (!format.hasColor) {
-			return this;
-		}
-
-		checkNewQuad();
-		current.vertices[vertex].color[0] = red / 255F;
-		current.vertices[vertex].color[1] = green / 255F;
-		current.vertices[vertex].color[2] = blue / 255F;
-		current.vertices[vertex].color[3] = alpha / 255F;
-		return this;
-	}
-
-	@Override
-	public IVertexBuilder uv(float u, float v) {
-		if (!format.hasUV) {
-			return this;
-		}
-
-		checkNewQuad();
-		current.vertices[vertex].uv[0] = u;
-		current.vertices[vertex].uv[1] = v;
-		return this;
-	}
-
-	@Override
-	public IVertexBuilder overlayCoords(int u, int v) {
-		if (!format.hasUV) {
-			return this;
-		}
-
-		checkNewQuad();
-		current.vertices[vertex].overlay[0] = u / (float) 0xF0;
-		current.vertices[vertex].overlay[1] = v / (float) 0xF0;
-		return this;
-	}
-
-	@Override
-	public IVertexBuilder uv2(int u, int v) {
-		if (!format.hasUV) {
-			return this;
-		}
-
-		checkNewQuad();
-		current.vertices[vertex].lightmap[0] = u / (float) 0xF0;
-		current.vertices[vertex].lightmap[1] = v / (float) 0xF0;
-		return this;
-	}
-
-	@Override
-	public IVertexBuilder normal(float x, float y, float z) {
-		if (!format.hasNormal) {
-			return this;
-		}
-
-		checkNewQuad();
-		current.vertices[vertex].normal[0] = x;
-		current.vertices[vertex].normal[1] = y;
-		current.vertices[vertex].normal[2] = z;
-		return this;
-	}
-
-	@Override
-	public void endVertex() {
-		vertex++;
-		if (vertex == vSize) {
-			if (glMode == GL11.GL_TRIANGLES) {
-				//Quadulate.
-				for (int e = 0; e < current.format.elementCount; e++) {
-					System.arraycopy(current.vertices[2].raw[e], 0, current.vertices[3].raw[e], 0, 4);
-				}
-			}
-			if (current.sprite == null) {
-				throw new IllegalStateException("Sprite not set.");
-			}
-			quadList.add(current);
-			current = null;
-			vertex = 0;
-		}
-	}
-
-	public List<BakedQuad> bake() {
-		if (current != null) {
-			throw new IllegalStateException("Not finished building.");
-		}
-
-		final ArrayList<BakedQuad> quads = new ArrayList<>();
-
-		for (final Quad unbakedQuad : quadList) {
-			quads.add(unbakedQuad.bake());
-		}
-
-		return quads;
-	}
-
-	private void checkNewQuad() {
-		if (current == null) {
-			current = new Quad(format);
-		}
-	}
+    private void checkNewQuad() {
+        if (current == null) {
+            current = new Quad(format);
+        }
+    }
 
 }
