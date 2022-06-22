f7cea35e8ad5a45d5d7b19c71986929232abdd80
covers1624 <laughlan.cov@internode.on.net>
2022-06-22 19:47:13 +0930

    Fix overly specific type on RayTracer.retraceBlock

--- src/main/java/codechicken/lib/raytracer/RayTracer.java
+++ src/main/java/codechicken/lib/raytracer/RayTracer.java
@@ -1,10 +1,12 @@
 package codechicken.lib.raytracer;
 
 import codechicken.lib.vec.Vector3;
+import net.covers1624.quack.annotation.ReplaceWith;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.state.BlockState;
@@ -18,14 +20,20 @@ import net.minecraftforge.common.ForgeMod;
 
 public class RayTracer {
 
-    public static BlockHitResult retraceBlock(LevelReader world, Player player, BlockPos pos) {
+
+    @Deprecated (forRemoval = true, since = "1.18.2") // Unnecessarily specific level type.
+    public static BlockHitResult retraceBlock(LevelReader level, Player player, BlockPos pos) {
+        return retraceBlock((BlockGetter) level, player, pos);
+    }
+
+    public static BlockHitResult retraceBlock(BlockGetter level, Player player, BlockPos pos) {
         Vec3 startVec = getStartVec(player);
         Vec3 endVec = getEndVec(player);
-        BlockState state = world.getBlockState(pos);
-        VoxelShape baseShape = state.getShape(world, pos);
+        BlockState state = level.getBlockState(pos);
+        VoxelShape baseShape = state.getShape(level, pos);
         BlockHitResult baseTraceResult = baseShape.clip(startVec, endVec, pos);
         if (baseTraceResult != null) {
-            BlockHitResult raytraceTraceShape = state.getVisualShape(world, pos, CollisionContext.of(player)).clip(startVec, endVec, pos);
+            BlockHitResult raytraceTraceShape = state.getVisualShape(level, pos, CollisionContext.of(player)).clip(startVec, endVec, pos);
             if (raytraceTraceShape != null) {
                 return raytraceTraceShape;
             }
